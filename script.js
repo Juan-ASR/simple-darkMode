3fa1bf48a02c459b6ed32cb5c88c68a21add1891
JuanASR <164096656+Juan-ASR@users.noreply.github.com>
2024-08-16 18:13:58 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function modoOscuro(){
+    document.body.style.backgroundColor = '#333';
+    document.body.style.color = 'white';
+    document.getElementById('container').style.color = 'white'
+    document.getElementById('btn-oscuro').style.backgroundColor = 'white'
+    document.getElementById('container').style.color = 'white'
+    document.getElementById('container').style.backgroundColor = 'white'
+    document.getElementById('container').style.color = 'white'
+
+}
+
+function modoClaro(){
+    document.body.style.backgroundColor = 'white';
+    document.body.style.color = 'black';
+    document.getElementById('container').style.color = 'black'
+    document.getElementById('btn-oscuro').style.backgroundColor = '#eee'
+    document.getElementById('container').style.color = 'black'
+    document.getElementById('container').style.backgroundColor = '#eee'
+    document.getElementById('container').style.color = 'black'
+}
